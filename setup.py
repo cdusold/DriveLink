1d35ea3caf2f954a31b3949a70ad5f9b0ddbe27d
Chris Dusold <cdusold@vt.edu>
2017-09-06 20:23:07 -0400

    Updated bugfix number.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="DriveLink",
-    version="0.2.1.0",
+    version="0.2.1.1",
     author="Chris Dusold",
     author_email="DriveLink@ChrisDusold.com",
     description=("A set of memory conserving data structures."),
